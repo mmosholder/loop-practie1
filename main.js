fe03ada54b90974b67f0a72440c61b8ee38f5138
Malinda Mosholder <m.m.mosholder@gmail.com>
2015-04-13 10:46:55 -0600

    initial work on exercises

--- main.js
+++ main.js
@@ -1,39 +1,41 @@
-// Answer to 1
-for(var i = 0; i <= 500; i = i + 100) {
-	console.log(i);
-}
+// // Answer to 1
+// for(var i = 0; i <= 500; i = i + 100) {
+// 	console.log(i);
+// }
 
-// Answer to 2
-for(var i = 1; i <= 64; i = i + i) {
-	console.log(i);
-}
+// // Answer to 2
+// for(var i = 1; i <= 64; i = i + i) {
+// 	console.log(i);
+// }
 
-// Answer to 3
-for(var i = 1; i < 4; i++) {
-	for(var j = 1; j < 4; j++) {
-	console.log(i);
-	}
-}
+// // Answer to 3
+// for(var i = 1; i < 4; i++) {
+// 	for(var j = 1; j < 4; j++) {
+// 	console.log(i);
+// 	}
+// }
 
 
-// Answer to 4
-for(var i = 0; i <= 10; i = i + 2) {
-	console.log(i);
-}
+// // Answer to 4
+// for(var i = 0; i <= 10; i = i + 2) {
+// 	console.log(i);
+// }
 
-// Answer to 5
-for(var i = 3; i <= 15; i = i + 3) {
-	console.log(i);
-}
+// // Answer to 5
+// for(var i = 3; i <= 15; i = i + 3) {
+// 	console.log(i);
+// }
 
-// Answer to 6
-for(var i = 9; i >= 0; i = i -1) {
-	console.log(i);
-}
+// // Answer to 6
+// for(var i = 9; i >= 0; i = i -1) {
+// 	console.log(i);
+// }
 
-// Answer to 7
-for(var i = 0; i < 3; i++) {
-	for(var j = 0; j < 4; j++) {
-		console.log(j);
-	}
-}
+// // Answer to 7
+// for(var i = 0; i < 3; i++) {
+// 	for(var j = 0; j < 4; j++) {
+// 		console.log(j);
+// 	}
+// }
+
+ 
